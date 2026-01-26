4cbc4a86b18a671ac41bc8eb83ccb4e9f17d9ffc
Lelpoco2 <a.divizia.tfi@gmail.com>
2026-01-26 17:52:10 +0100

    .

--- demo_item_list.py
+++ demo_item_list.py
@@ -13,15 +13,15 @@ if __name__ == "__main__":
         BoxType("Pacco-S", 12.5, 10, 26.5, 0.5, max_boxes=5),
         BoxType("Pacco-M", 11, 20, 27, 1.0, max_boxes=5),
         # Uncomment these if you want to test XL/XXL
-        # BoxType("Pacco-XL", 30, 30, 30, 1.5, max_boxes=3),
-        # BoxType("Pacco-XXL", 40, 40, 40, 2.0, max_boxes=3),
+        BoxType("Pacco-XL", 30, 30, 30, 1.5, max_boxes=3),
+        BoxType("Pacco-XXL", 40, 40, 40, 2.0, max_boxes=3),
     ]
 
     # Define a LIST of different items to pack
     items_to_pack = [
-        ItemType("MyPOS:Go2", 11, 18.5, 8.5, quantity=7),
-        ItemType("Phone", 15, 8, 1.5, quantity=3),
-        ItemType("Charger", 5, 5, 2, quantity=16),
+        ItemType("MyPOS:Go2", 11, 18.5, 8.5, quantity=10),
+        # ItemType("Phone", 15, 8, 1.5, quantity=3),
+        # ItemType("Charger", 5, 5, 2, quantity=16),
     ]
 
     # Pack all items
